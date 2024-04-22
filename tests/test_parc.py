6c9127e4b2f95408885f4895fa159d2784aee042
Ainsleigh Hill <ainsleighhill@gmail.com>
2024-04-22 14:58:05 -0700

    update test_parc

--- tests/test_parc.py
+++ tests/test_parc.py
@@ -1,5 +1,6 @@
 import pytest
 from sklearn import datasets
+import time
 from parc._parc import PARC
 from parc.logger import get_logger
 
@@ -14,6 +15,21 @@ def iris_data():
     return x_data, y_data
 
 
+@pytest.fixture
+def forest_data():
+    forests = datasets.fetch_covtype()
+    x_data = forests.data[list(range(0, 30000)), :]
+    y_data = forests.target[list(range(0, 30000))]
+    return x_data, y_data
+
+
+@pytest.mark.parametrize(
+    "dataset_name, f1_mean, f1_accumulated, run_time",
+    [
+        ("iris_data", 0.9, 0.9, 1),
+        ("forest_data", 0.6, 0.6, 10)
+    ]
+)
 @pytest.mark.parametrize(
     "targets_exist",
     [
@@ -21,12 +37,20 @@ def iris_data():
         (False)
     ]
 )
-def test_parc_run_parc(iris_data, targets_exist):
-    x_data, y_data = iris_data
+def test_parc_run_parc(request, dataset_name, f1_mean, f1_accumulated, run_time, targets_exist):
+    x_data, y_data = request.getfixturevalue(dataset_name)
     if targets_exist:
         parc_model = PARC(x_data, y_data_true=y_data)
     else:
         parc_model = PARC(x_data)
+    start_time = time.time()
     parc_model.run_parc()
-    logger.info(parc_model.y_data_pred)
-    assert len(parc_model.y_data_pred) == 150
+    end_time = time.time()
+    assert end_time - start_time < run_time
+    if targets_exist:
+        assert parc_model.f1_mean >= f1_mean
+        assert parc_model.f1_accumulated >= f1_accumulated
+    else:
+        assert parc_model.f1_mean == 0
+        assert parc_model.f1_accumulated == 0
+    assert len(parc_model.y_data_pred) == y_data.shape[0]
