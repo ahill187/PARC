0a372e50d5c0ba07ed6633b7f7280413ceb6ed19
Ainsleigh Hill <ainsleighhill@gmail.com>
2024-05-09 20:37:58 -0700

    rename cluster_ii to community_id

--- parc/_parc.py
+++ parc/_parc.py
@@ -561,15 +561,15 @@ class PARC:
             set_node_communities = set(node_communities)
 
             node_communities = np.asarray(node_communities)
-            for cluster_ii in set_node_communities:
-                cluster_ii_loc = np.where(node_communities == cluster_ii)[0]
+            for community_id in set_node_communities:
+                cluster_ii_loc = np.where(node_communities == community_id)[0]
                 community_size = len(cluster_ii_loc)
                 not_yet_expanded = community_size not in list_pop_too_bigs
                 if community_size > large_community_factor * n_samples and not_yet_expanded:
                     too_big = True
-                    logger.info(f"Cluster {cluster_ii} is too big and has population {community_size}.")
+                    logger.info(f"Cluster {community_id} is too big and has population {community_size}.")
                     cluster_big_loc = cluster_ii_loc
-                    cluster_big = cluster_ii
+                    cluster_big = community_id
                     big_pop = community_size
             if too_big:
                 list_pop_too_bigs.append(big_pop)
