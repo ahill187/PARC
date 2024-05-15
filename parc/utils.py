bf0473465b71094750df2aad63e0ccd0477df5ec
Ainsleigh Hill <ainsleighhill@gmail.com>
2024-05-15 15:03:30 -0700

    create memory functions

--- parc/utils.py
+++ parc/utils.py
@@ -1,3 +1,10 @@
+import os
+import psutil
+
+MEMORY_THRESHOLD = 0.0625 # GiB
+MEMORY_PRUNE_GLOBAL = 0.184 / 10**6 # GiB / edge
+
+
 def get_mode(a_list):
     """Get the value which appears most often in the list (the mode).
 
@@ -10,3 +17,21 @@ def get_mode(a_list):
         the most frequent value in the list
     """
     return max(set(a_list), key=a_list.count)
+
+
+def get_current_memory_usage(process):
+    return process.memory_info().rss / (1024**3)
+
+
+def get_available_memory():
+    return psutil.virtual_memory().available / (1024**3)
+
+
+def get_memory_prune_global(n_edges):
+    required_memory = MEMORY_PRUNE_GLOBAL * n_edges
+    available_memory = get_available_memory()
+    return {
+        "required": required_memory,
+        "available": available_memory,
+        "is_sufficient": available_memory - required_memory - MEMORY_THRESHOLD >= 0
+    }
