b485cc7bb1f3208a6b94fc1415a8fd9e349b8386
Ainsleigh <ainsleighhill@gmail.com>
2024-08-11 14:24:25 -0700

    modify get_mode docstring

--- parc/utils.py
+++ parc/utils.py
@@ -1,7 +1,8 @@
 def get_mode(a_list: list[any]) -> any:
     """Get the value which appears most often in the list (the mode).
 
-    If multiple items are maximal, the function returns the first one encountered.
+    If multiple items are maximal, the function returns the first one encountered
+    (not necessarily the first one in the list).
 
     Args:
         a_list: A list with values.
