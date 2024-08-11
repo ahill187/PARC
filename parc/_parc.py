68042acb72387852ee9943cc6b02d2f5f8a079f5
Ainsleigh <ainsleighhill@gmail.com>
2024-08-11 14:13:53 -0700

    move func_mode to utils

--- parc/_parc.py
+++ parc/_parc.py
@@ -6,6 +6,7 @@ import igraph as ig
 import leidenalg
 import time
 from umap.umap_ import find_ab_params, simplicial_set_embedding
+from parc.utils import get_mode
 from parc.logger import get_logger
 
 
@@ -192,10 +193,6 @@ class PARC:
             shape=(n_cells, n_cells)
         )
         return csr_graph
-
-    def func_mode(self, ll):  # return MODE of list
-        # If multiple items are maximal, the function returns the first one encountered.
-        return max(set(ll), key=ll.count)
 
     def run_toobig_subPARC(
         self,
@@ -551,7 +548,7 @@ class PARC:
 
         for kk in sorted_keys:
             vals = [t for t in Index_dict[kk]]
-            majority_val = self.func_mode(vals)
+            majority_val = get_mode(vals)
             if majority_val == onevsall:
                 logger.message(f"Cluster {kk} has majority {onevsall} with population {len(vals)}")
             if kk == -1:
@@ -596,7 +593,7 @@ class PARC:
         for cluster_i in set(PARC_labels):
             cluster_i_loc = np.where(np.asarray(PARC_labels) == cluster_i)[0]
             true_labels = np.asarray(true_labels)
-            majority_truth = self.func_mode(list(true_labels[cluster_i_loc]))
+            majority_truth = get_mode(list(true_labels[cluster_i_loc]))
             majority_truth_labels[cluster_i_loc] = majority_truth
 
         majority_truth_labels = list(majority_truth_labels.flatten())
