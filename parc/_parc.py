7e6982d784f9ddc1a7c8e8c8c7a1ce25b378088c
Ainsleigh <ainsleighhill@gmail.com>
2024-08-12 13:07:16 -0700

    rename `num_threads` -> `n_threads`

--- parc/_parc.py
+++ parc/_parc.py
@@ -27,7 +27,7 @@ class PARC:
         knn=30,
         n_iter_leiden=5,
         random_seed=42,
-        num_threads=-1,
+        n_threads=-1,
         distance="l2",
         time_smallpop=15,
         partition_type="ModularityVP",
@@ -56,7 +56,7 @@ class PARC:
         self.knn = knn
         self.n_iter_leiden = n_iter_leiden #the default is 5 in PARC
         self.random_seed = random_seed  # enable reproducible Leiden clustering
-        self.num_threads = num_threads  # number of threads used in KNN search/construction
+        self.n_threads = n_threads  # number of threads used in KNN search/construction
         self.distance = distance  # Euclidean distance "l2" by default; other options "ip" and "cosine"
         self.time_smallpop = time_smallpop #number of seconds trying to check an outlier
         self.partition_type = partition_type #default is the simple ModularityVertexPartition where resolution_parameter =1. In order to change resolution_parameter, we switch to RBConfigurationVP
@@ -75,7 +75,7 @@ class PARC:
             num_dims = self.x_data.shape[1]
             n_elements = self.x_data.shape[0]
             p = hnswlib.Index(space=self.distance, dim=num_dims)  # default to Euclidean distance
-            p.set_num_threads(self.num_threads)  # set threads used in KNN construction
+            p.set_num_threads(self.n_threads)  # set threads used in KNN construction
             if n_elements < 10000:
                 ef_query = min(n_elements - 10, 500)
                 ef_construction = ef_query
