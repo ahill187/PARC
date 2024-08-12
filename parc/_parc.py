678fa348ee64398bb196a4e5cbdd5574fe05e5f7
Ainsleigh <ainsleighhill@gmail.com>
2024-08-12 16:00:41 -0700

    clean up create_knn_graph

--- parc/_parc.py
+++ parc/_parc.py
@@ -236,9 +236,11 @@ class PARC:
             A compressed sparse row matrix with dimensions ``(n_samples, n_samples)``,
             containing the pruned distances.
         """
+
         k_umap = 15
+
         # neighbors in array are not listed in in any order of proximity
-        self.knn_struct.set_ef(k_umap+1)
+        self.knn_struct.set_ef(k_umap + 1)
         neighbor_array, distance_array = self.knn_struct.knn_query(self.x_data, k=k_umap)
 
         row_list = []
@@ -252,27 +254,21 @@ class PARC:
         row_min = np.min(distance_array, axis=1)
         row_sigma = np.std(distance_array, axis=1)
 
-        distance_array = (distance_array - row_min[:, np.newaxis])/row_sigma[:, np.newaxis]
+        distance_array = (distance_array - row_min[:, np.newaxis]) / row_sigma[:, np.newaxis]
+        distance_array = np.sqrt(distance_array.flatten()) * -1
 
         col_list = neighbor_array.flatten().tolist()
-        distance_array = distance_array.flatten()
-        distance_array = np.sqrt(distance_array)
-        distance_array = distance_array * -1
 
         weight_list = np.exp(distance_array)
-
         threshold = np.mean(weight_list) + 2 * np.std(weight_list)
-
         weight_list[weight_list >= threshold] = threshold
 
-        weight_list = weight_list.tolist()
-
-        graph = csr_matrix((np.array(weight_list), (np.array(row_list), np.array(col_list))),
-                           shape=(n_samples, n_samples))
-
+        graph = csr_matrix(
+            (weight_list, (np.array(row_list), np.array(col_list))),
+            shape=(n_samples, n_samples)
+        )
         graph_transpose = graph.T
         prod_matrix = graph.multiply(graph_transpose)
-
         graph = graph_transpose + graph - prod_matrix
         return graph
 
