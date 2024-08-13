dc35c8e2fc38e689da8e014faf31ef5063fd873f
Ainsleigh <ainsleighhill@gmail.com>
2024-08-12 17:07:50 -0700

    add tests for create_knn_graph

--- tests/test_parc.py
+++ tests/test_parc.py
@@ -1,7 +1,11 @@
 import pytest
 from sklearn import datasets
 import igraph as ig
+import numpy as np
 from parc._parc import PARC
+from parc.logger import get_logger
+
+logger = get_logger(__name__, 20)
 
 
 @pytest.fixture
@@ -49,3 +53,15 @@ def test_parc_get_leiden_partition(iris_data, knn, jac_weighted_edges):
     assert len(leiden_partition.membership) == len(y_data)
     assert len(leiden_partition) <= len(y_data)
     assert len(leiden_partition) >= 1
+
+
+@pytest.mark.parametrize("knn", [5, 10, 20, 50])
+def test_parc_create_knn_graph(iris_data, knn):
+    x_data = iris_data[0]
+    y_data = iris_data[1]
+
+    parc_model = PARC(x_data, y_data_true=y_data)
+    parc_model.knn_struct = parc_model.make_knn_struct()
+    csr_array = parc_model.create_knn_graph(knn=knn)
+    nn_collection = np.split(csr_array.indices, csr_array.indptr)[1:-1]
+    assert len(nn_collection) == y_data.shape[0]
